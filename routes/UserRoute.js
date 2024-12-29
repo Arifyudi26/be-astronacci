ec837e34bc3509bc51dd07d7313343335a7908a9
arifYudi <arif.yudisuryo@sigma-tech.co.id>
2024-12-29 19:07:09 +0700

    feat: enhance user update functionality with alerts and error handling
    - Implement success and error alerts using SweetAlert2
    - Redirect user to homepage upon successful update
    - Display dynamic error messages from API responses
    - Add validation to ensure no unchanged data is submitted

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   getUsers,
   getUserById,
+  updateUserById,
 } from "../controllers/UsersControllers.js";
 import { jwtAuth } from "../middleware/AuthUser.js";
 
@@ -9,5 +10,6 @@ const router = express.Router();
 
 router.get("/users", jwtAuth, getUsers);
 router.get("/users/:id", jwtAuth, getUserById);
+router.put("/users/:id", jwtAuth, updateUserById);
 
 export default router;
