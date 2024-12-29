196dffc3ca0852bb8e28a0837e406b588ec0b555
arifYudi <arif.yudisuryo@sigma-tech.co.id>
2024-12-29 19:07:09 +0700

    feat: improve user update functionality with API error handling and validation
    - Implement dynamic error handling for user update API
    - Add validation to ensure only modified data is submitted
    - Enhance response structure for success and error cases
    - Update API to handle missing or invalid data more effectively

--- controllers/UsersControllers.js
+++ controllers/UsersControllers.js
@@ -1,5 +1,4 @@
 import User from "../models/UserModel.js";
-import argon2 from "argon2";
 
 export const getUsers = async (req, res) => {
   try {
@@ -14,13 +13,69 @@ export const getUsers = async (req, res) => {
 
 export const getUserById = async (req, res) => {
   try {
-    const response = await User.findOne({
-      attributes: ["id", "name", "email", "type_membership"],
+    const user = await User.findOne({
+      attributes: ["id", "name", "email", "type_membership", "bio"],
       where: {
         id: req.params.id,
       },
     });
-    res.status(200).json(response);
+
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const updateUserById = async (req, res) => {
+  try {
+    const user = await User.findOne({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    const { name, email, type_membership, bio } = req.body;
+
+    if (!name) return res.status(400).json({ message: "Name is required" });
+    if (!email) return res.status(400).json({ message: "Email is required" });
+    if (!type_membership)
+      return res.status(400).json({ message: "Type Membership is required" });
+    if (!bio) return res.status(400).json({ message: "Bio is required" });
+
+    // Check whether the input data is the same as the old data
+    const isSameData =
+      user.name === name &&
+      user.email === email &&
+      user.type_membership === type_membership &&
+      user.bio === bio;
+
+    if (isSameData) {
+      return res
+        .status(400)
+        .json({ message: "No changes detected. Update failed." });
+    }
+
+    await user.update({
+      name: name,
+      email: email,
+      type_membership: type_membership,
+      bio: bio,
+    });
+
+    res.status(200).json({
+      message: "User updated successfully",
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        type_membership: user.type_membership,
+        bio: user.bio,
+      },
+    });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
